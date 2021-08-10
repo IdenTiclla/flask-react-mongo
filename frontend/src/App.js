3861793171c1b528edbf4999b3b89b15aec929bf
unknown <iden.ticlla@gmail.com>
2021-08-10 09:20:46 -0400

    add Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+function App() {
+  return (
+    <Router>
+        <div>
+            <Switch>
+                <Route path="/about" component={}/>
+            </Switch>
+        </div>
+    </Router>
+  );
+}
+
+export default App;
