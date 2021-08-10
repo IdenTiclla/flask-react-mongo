a99ae8e13855286c9da45689ba0452572100463c
unknown <iden.ticlla@gmail.com>
2021-08-10 17:36:43 -0400

    create user completed

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,6 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const Users = () => (
-    <h1>Users</h1>
-)
+const API = process.env.REACT_APP_API
+export const Users = () => {
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await fetch(`${API}/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
+    return (
+    <div className="row">
+        <div className="col md-4">
+            <form onSubmit={handleSubmit} className="card card-body">
+                <div className="form-group">
+                    <input 
+                        type="text"
+                        onChange={e => setName(e.target.value)}
+                        value={name}
+                        className="form-control"
+                        placeholder="Name"
+                        autoFocus
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input 
+                        type="email"
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
+                        className="form-control"
+                        placeholder="Email"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input 
+                        type="password"
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
+                        className="form-control"
+                        placeholder="Password"
+                        autoFocus
+                    />
+                </div>
+                <button className="btn btn-primary btn-block">
+                    Create
+                </button>
+            </form>
+        </div>
+        <div className="col md-8">
+
+        </div>
+    </div>
+    )
+}
