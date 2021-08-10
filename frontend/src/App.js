3cca4328b7bc087951164b55956148d8679c1c90
unknown <iden.ticlla@gmail.com>
2021-08-10 14:00:02 -0400

    add Navbar and bootstrap styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import {About} from './components/About'
 import {Users} from './components/Users'
+import {Navbar} from './components/Navbar'
 
 function App() {
   return (
     <Router>
-        <div>
+        <Navbar/>
+        <div className="container p-4">
             <Switch>
                 <Route path="/about" component={About}/>
                 <Route path="/" component={Users}/>
